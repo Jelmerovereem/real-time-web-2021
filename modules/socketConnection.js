23ea557a322cf2696442daf6f59e8364e81d96b7
Jelmer Overeem <jelmer_overeem@hotmail.com>
2021-04-20 11:34:14 +0200

    updated socket

--- modules/socketConnection.js
+++ modules/socketConnection.js
@@ -1,21 +1,48 @@
 import { io }  from "../server.js";
+import addUser from "./addUserDB.js";
+import updateUser from "./updateUserDB.js";
+import updateAdminLiveUsers from "./updateAdminLiveUsers.js";
+import * as fs from "fs";
 
-export default function socketHandling() {
+export default function socketHandling(req, res) {
 	let adminChannel = io.of("/adminChannel");
 
 	let connectedUsers = 0;
 	io.on("connection", (socket) => {
+		let liveUser = {};
+
 		connectedUsers++;
 		io.emit("usersConnected", connectedUsers);
 		socket.on("disconnect", () => {
-			connectedUsers--;
-			io.emit("usersConnected", connectedUsers);
+			liveUser.disconnected = true;
+			setTimeout(() => {
+				connectedUsers--;
+				io.emit("usersConnected", connectedUsers);
+				console.log("updating admin on user disconnect")
+				updateAdminLiveUsers(connectedUsers);
+			}, 10000)
 		})
 
-		socket.on("pageChange", (userData) => {
-			userData.socketID = socket.id;
-			console.log(`user: ${userData.userId} and ${userData.socketID} went to ${userData.page}`);
-			adminChannel.emit("alertAdmin", userData);
+		socket.on("pageChange", async (userData) => {
+			const sHeaders = socket.handshake.headers;
+			const clientIp = socket.request.connection.remoteAddress;
+			console.log(clientIp);
+			console.log(sHeaders['x-forwarded-for'], sHeaders['x-forwarded-port']);
+			userData.socketId = socket.id;
+			if (userData.userId) {
+				liveUser = userData;
+				liveUser.disconnected = false;
+			}
+			const response = await updateUser(userData, socket);
+			if (response === "ok") {
+				console.log("updating admin live on pageChange")
+				updateAdminLiveUsers(connectedUsers);
+			}
+			//adminChannel.emit("alertAdmin", userData);
 		})
+	})
+	adminChannel.on("connection", (socket) => {
+		console.log("admin connected")
+		updateAdminLiveUsers(connectedUsers)
 	})
 }
